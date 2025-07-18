0a491d2f9dc90ce77ffa1b727f09ae3b73e40354
Kasia Biernat <kasia.biernat@hyland.com>
2025-07-18 13:48:14 +0200

    update plugins to json

--- apps/my-app/src/app/home.component.ts
+++ apps/my-app/src/app/home.component.ts
@@ -3,30 +3,24 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
-import {MatRippleModule} from '@angular/material/core';
-
-export interface App {
-  name: string;
-  subtitle: string;
-  description: string;
-  route?: string;
-}
+import plugins from './plugins.json';
+import { Plugin } from './plugin-interface';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatIconModule, MatRippleModule, RouterModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, RouterModule],
   template: `
     <div class="home-container">
-      @for (app of apps; track app.name) {
-        <mat-card matRipple class="app-card" [class.inactive]="!app.route" [routerLink]="app.route">
+      @for (plugin of plugins; track plugin.name) {
+        <mat-card class="card" [class.inactive]="!plugin.route" [routerLink]="plugin.route">
           <mat-card-header>
-            <mat-card-title>{{ app.name }}</mat-card-title>
-            <mat-card-subtitle>{{ app.subtitle }}</mat-card-subtitle>
+            <mat-card-title>{{ plugin.name }}</mat-card-title>
+            <mat-card-subtitle>{{ plugin.subtitle }}</mat-card-subtitle>
           </mat-card-header>
           <mat-card-content>
             <p>
-              {{ app.description }}
+              {{ plugin.description }}
             </p>
           </mat-card-content>
           <mat-card-actions>
@@ -56,16 +50,16 @@ export interface App {
       gap: 1rem;
     }
 
-    .app-card:not(.inactive) {
+    .card:not(.inactive) {
       transition: transform 0.2s ease-in-out;
       cursor: pointer;
     }
 
-    .app-card:not(.inactive):hover {
+    .card:not(.inactive):hover {
       transform: scale(1.03);
     }
 
-    .app-card.inactive {
+    .card.inactive {
       opacity: 0.5;
       pointer-events: none;
     }
@@ -73,25 +67,5 @@ export interface App {
   `],
 })
 export class HomeComponent {
-  apps = [
-    {
-      name: 'Document List',
-      subtitle: 'Manage the company documents.',
-      description:
-        'This application allows you to view and manage documents. You can add, edit, and delete documents as needed. It provides a user-friendly interface for document management.',
-      route: '/document-list',
-    },
-    {
-      name: 'Unimplemented Plugin 1',
-      subtitle: 'A simple plugin 1 application.',
-      description:
-        'This is a simple plugin application that demonstrates basic functionality. It can be extended with additional features as needed.',
-    },
-    {
-      name: 'Unimplemented Plugin 2',
-      subtitle: 'A simple plugin 2 application.',
-      description:
-        'This is a simple plugin application that demonstrates basic functionality. It can be extended with additional features as needed.',
-    },
-  ];
+  plugins: Plugin[] = plugins;
 }
